767db3a10319c7611916001da4729c6818d16b60
ktbh4jc <37726886+ktbh4jc@users.noreply.github.com>
2024-05-07 23:36:18 -0500

    feat: OM-01 send order to OrderManager (#4)
    * feat: OM-01 send order to OrderManager
    
    * chore: add thunder client collection

--- api/producers.go
+++ api/producers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
@@ -26,16 +27,54 @@ func NewHelloProducerImpl(p *kafka.Producer, topic string) *HelloProducerImpl {
 
 func (hp *HelloProducerImpl) SayHello() error {
 	payload := []byte("Hello world")
-	message := &kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &hp.topic, Partition: kafka.PartitionAny},
-		Value:          payload}
+	message := buildMessage(hp.topic, payload)
 
 	err := hp.producer.Produce(message, hp.deliveryChan)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	log.Printf("Sending payload %v\n", payload)
+	log.Printf("Sending payload %s\n", string(payload))
 	<-hp.deliveryChan
 	return nil
+}
+
+type OrderProducer interface {
+	PlaceOrder(OrderRequest) error
+}
+
+type OrderProducerImpl struct {
+	producer     *kafka.Producer
+	topic        string
+	deliveryChan chan kafka.Event
+}
+
+func NewOrderProducerImpl(p *kafka.Producer, topic string) *OrderProducerImpl {
+	return &OrderProducerImpl{
+		producer:     p,
+		topic:        topic,
+		deliveryChan: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *OrderProducerImpl) PlaceOrder(request OrderRequest) error {
+	payload, err := json.Marshal(request)
+	if err != nil {
+		return err
+	}
+	message := buildMessage(op.topic, payload)
+	err = op.producer.Produce(message, op.deliveryChan)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Sending order %s\n", string(payload))
+	<-op.deliveryChan
+	return nil
+}
+
+func buildMessage(topic string, payload []byte) *kafka.Message {
+	return &kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          payload}
 }
